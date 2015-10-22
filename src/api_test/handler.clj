996e61adce635c61ef299ba2806cfc97e620f4dc
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2015-10-22 23:28:06 +0300

    test subcontexts

--- src/api_test/handler.clj
+++ src/api_test/handler.clj
@@ -3,18 +3,24 @@
             [ring.util.http-response :refer :all]
             [schema.core :as s]))
 
-(s/defschema Message {:message String})
-
 (defapi app
-  (swagger-ui)
+  (swagger-ui "/apidocs")
   (swagger-docs
-    {:info {:title "Api-test"
-            :description "Compojure Api example"}
-     :tags [{:name "hello", :description "says hello in Finnish"}]})
-  (context* "/hello" []
-    :tags ["hello"]
-    (GET* "/" []
-      :return Message
-      :query-params [name :- String]
-      :summary "say hello"
-      (ok {:message (str "Terve, " name)}))))
+   {:info {:title "Oma API"
+           :version "0.1"
+           :description "sitä sun tätä api"}
+    :tags [{:name "fi" :description "Finnish language"}
+           {:name "se" :description "Swedish language"}]})
+  (context* "/api/v1" []
+            (context* "/fi" []
+                      :tags ["fi"]
+                      (GET* "/hello" []
+                            :query-params [name :- String]
+                            :summary "say hello"
+                            (ok {:message (str "Terve, " name)})))
+            (context* "/se" []
+                     :tags ["se"]
+                     (GET* "/hello" []
+                           :query-params [name :- String]
+                           :summary "say hello"
+                           (ok {:message (str "Hejsan, " name)})))))

8afae23e0aeff89366b6fec76087869decb220c8
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2015-10-23 09:51:42 +0300

    use lein 2.0.0

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [metosin/compojure-api "0.22.0"]]
   :ring {:handler api-test.handler/app}
+  :min-lein-version "2.0.0"
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
                    :plugins [[lein-ring "0.9.6"]]}})
